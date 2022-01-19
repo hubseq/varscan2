80c99ca9c230aaf775c0ae27a9b09322d1873cad
EC2 Default User <ec2-user@ip-10-88-1-53.us-west-2.compute.internal>
2022-01-19 17:24:37 +0000

    “save”

--- src/run_main.py
+++ src/run_main.py
@@ -14,16 +14,18 @@ def runOtherPre( input_dir, output_dir, run_json ):
     """ This function is used to run any other commands BEFORE the main program has run.
     run_json has most of what you might need to run other commands, and has the following structure:
 
-    run_json = {'module': module_name, 'local_input_dir': <LOCAL_INPUT_DIR>, 'local_output_dir': <LOCAL_OUT_DIR>, \
+    run_json = {'module': module_name, 'run_job_id': run_job_id', \
+                'local_input_dir': <LOCAL_INPUT_DIR>, 'local_output_dir': <LOCAL_OUT_DIR>, \
 		'remote_input_dir': remote_input_directory, 'remote_output_dir': remote_output_directory, \
+                'local_input_file': local_input_file, 'local_output_file': local_output_file, \
                 'program_arguments': program_arguments, 'run_arguments': run_arguments_json, \
-                'module_instance_json': module_instance_json}
-
+                'module_instance_json': module_instance_json, 'job_json': job_json}
+    
     LOCAL_INPUT_DIR has any downloaded files. LOCAL_OUT_DIR has any output data or log files that will be uploaded.
 
     If you are not running any other commands or post-processing, then leave this function blank.
     """
-    return
+    return run_json
 
 
 def runOtherPost( input_dir, output_dir, run_json ):
@@ -32,7 +34,7 @@ def runOtherPost( input_dir, output_dir, run_json ):
 
     If you are not running any other commands or pre-processing, then leave this function blank.
     """
-    return
+    return run_json
 
 
 def runMain():
@@ -44,13 +46,13 @@ def runMain():
     run_json = module_utils.initProgram()
     
     # do any pre-processing (specific to module)
-    runOtherPre( run_json['local_input_dir'], run_json['local_output_dir'], run_json )
+    run_json = runOtherPre( run_json['local_input_dir'], run_json['local_output_dir'], run_json )
     
     # run main program
     module_utils.runProgram( run_json['program_arguments'], run_json['local_output_file'] )
     
     # do any post-processing
-    runOtherPost( run_json['local_input_dir'], run_json['local_output_dir'], run_json )
+    run_json = runOtherPost( run_json['local_input_dir'], run_json['local_output_dir'], run_json )
 
     # create run log that includes program run duration
     run_end = datetime.now()
